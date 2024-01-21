a78f9b0cd4e9c88c5a002d533b2cee07bc3a2b31
Eric <eric.munozsoldado@gmail.com>
2024-01-21 23:59:51 +0100

    Fix to one day in milliseconds

--- src/utils/isMoreThanOneDayAgo.ts
+++ src/utils/isMoreThanOneDayAgo.ts
@@ -5,9 +5,7 @@ export interface IIsMoreThanOneDayAgo {
 }
 
 export const isMoreThanOneDayAgo: FC<IIsMoreThanOneDayAgo> = (lastFetchTime): boolean => {
-  const oneDayInMilliseconds = 10000
-  // Uncomment when finish QA and remove the line above
-  // const oneDayInMilliseconds = 24 * 60 * 60 * 1000
+  const oneDayInMilliseconds = 24 * 60 * 60 * 1000
   const currentTime = new Date().getTime()
 
   return currentTime - new Date(String(lastFetchTime)).getTime() > oneDayInMilliseconds
