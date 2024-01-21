5672cd31bf63754d5be7e9184c2b95e8d5d14515
Eric <eric.munozsoldado@gmail.com>
2024-01-21 12:27:47 +0100

    Solve List Slice TS types

--- src/store/slices/ListSlice.ts
+++ src/store/slices/ListSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk, type AsyncThunk } from '@reduxjs/toolkit'
 
 import { type RootState } from '../store'
 import { type IOompaLoompa } from '../../types/oompaLoompa'
@@ -21,14 +21,15 @@ const initialState: State = {
 
 const API_BASE_URL = 'https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas'
 
-export const fetchList = createAsyncThunk('FETCH_LIST', async (page: number) => {
-  const response = await fetch(`${API_BASE_URL}?page=${page}`)
-  if (!response.ok) {
-    throw new Error('Failed to fetch Oompa Loompa List')
-  }
-  const data = await response.json()
-  return data.results
-})
+export const fetchList: AsyncThunk<string, number, Record<string, unknown>> = createAsyncThunk(
+  'FETCH_LIST', async (page: number) => {
+    const response = await fetch(`${API_BASE_URL}?page=${page}`)
+    if (!response.ok) {
+      throw new Error('Failed to fetch Oompa Loompa List')
+    }
+    const data = await response.json()
+    return data.results
+  })
 
 export const ListSlice = createSlice({
   name: 'list',
@@ -51,20 +52,20 @@ export const ListSlice = createSlice({
       })
       .addCase(fetchList.fulfilled, (state, action) => {
         state.loading = false
-        state.data = [...state.data, ...action.payload]
+        state.data = [...state.data, ...action.payload] as unknown as IOompaLoompa[]
         state.currentPage = state.currentPage + 1
       })
       .addCase(fetchList.rejected, (state, action) => {
         state.loading = false
-        state.error = action.error.message || 'Something went wrong'
+        state.error = action.error.message ?? 'Something went wrong'
       })
   }
 })
 
-export const selectList = (state: RootState) => state.list.data
-export const selectCurrentPage = (state: RootState) => state.list.currentPage
-export const selectLastFetchTime = (state: RootState) => state.list.lastFetchTime
-export const selectLoading = (state: RootState) => state.list.loading
-export const selectError = (state: RootState) => state.list.error
+export const selectList = (state: RootState): IOompaLoompa[] => state.list.data
+export const selectCurrentPage = (state: RootState): number => state.list.currentPage
+export const selectLastFetchTime = (state: RootState): string | undefined => state.list.lastFetchTime
+export const selectLoading = (state: RootState): boolean => state.list.loading
+export const selectError = (state: RootState): string | null => state.list.error
 
 export const { setLastFetchTime, clearCache } = ListSlice.actions
