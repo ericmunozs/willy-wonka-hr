d35842e3b535347746f9b3741617cd484aa67c36
Eric <eric.munozsoldado@gmail.com>
2024-01-21 17:23:49 +0100

    Add lazy loading in HomePage

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,16 +1,19 @@
-// src/routes/index.tsx
-import { type FC } from 'react'
+import { Suspense, lazy, type FC } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { Layout } from '../components/organisms/Layout/Layout'
 import { DetailsPage } from '../pages/DetailsPage/DetailsPage'
-import { HomePage } from '../pages/HomePage/HomePage'
 import { NotFoundPage } from '../pages/NotFoundPage/NotFoundPage'
+
+const HomePage = lazy(async () => {
+  const module = await import('../pages/HomePage/HomePage')
+  return { default: module.HomePage }
+})
 
 export const AppRoutes: FC = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Layout><HomePage /></Layout>} />
+      <Route path="/" element={<Layout><Suspense fallback={<div>Loading...</div>}><HomePage /></Suspense></Layout>} />
       <Route path="/:id" element={<Layout><DetailsPage /></Layout>} />
       <Route path="/*" element={<Layout><NotFoundPage /></Layout>} />
     </Routes>
