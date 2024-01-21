9033d1f7444dc2aefa9c3e1cd331fb22e1eac530
Eric <eric.munozsoldado@gmail.com>
2024-01-21 17:23:01 +0100

    Fix: NavBar logo src

--- src/components/molecules/NavBar/NavBar.tsx
+++ src/components/molecules/NavBar/NavBar.tsx
@@ -6,7 +6,7 @@ import './NavBar.css'
 export const NavBar: FC = () => (
   <div className='navbar'>
     <Link to="/" className='navbar__link'>
-      <img src="src/assets/logo-oompa-loompa.png" alt="logo" />
+      <img src="/src/assets/logo-oompa-loompa.png" alt="logo" />
     </Link>
     <span className='navbar__title'>Oompa Lompa&apos;s Crew</span>
   </div>
