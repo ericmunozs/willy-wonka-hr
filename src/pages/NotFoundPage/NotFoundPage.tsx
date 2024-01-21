54df2589735b2ea650df3f1dc52780cddd16657b
Eric <eric.munozsoldado@gmail.com>
2024-01-21 15:53:23 +0100

    Add NotFoundPage

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+import { type FC } from 'react'
+
+export const NotFoundPage: FC = () => {
+  return (
+    <div>
+      <h2>404 - Page cannot be found</h2>
+      <p>Sorry, we&apos;re unable to bring you the page you&apos;re looking for.</p>
+    </div>
+  )
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,12 +5,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Layout } from '../components/organisms/Layout/Layout'
 import { DetailsPage } from '../pages/DetailsPage/DetailsPage'
 import { HomePage } from '../pages/HomePage/HomePage'
+import { NotFoundPage } from '../pages/NotFoundPage/NotFoundPage'
 
 export const AppRoutes: FC = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layout><HomePage /></Layout>} />
       <Route path="/:id" element={<Layout><DetailsPage /></Layout>} />
+      <Route path="/*" element={<Layout><NotFoundPage /></Layout>} />
     </Routes>
   </BrowserRouter>
 )
