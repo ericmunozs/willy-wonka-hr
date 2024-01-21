27f585ca4fdadb415770b203ddeb4af5d246f9e2
Eric <eric.munozsoldado@gmail.com>
2024-01-21 13:33:59 +0100

    Remove unused reducer

--- src/reducers/oompaLoompaReducer.ts
+++
@@ -1,8 +0,0 @@
-export const oompaLoompaReducers = (state = [], action) => {
-  switch (action.type) {
-    case 'FETCH_OOMPA_LOOMPAS':
-      return [...action.payload, ...state]
-    default:
-      return state
-  }
-}
